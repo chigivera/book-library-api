58c6e3018c8196751fd6062cd1bc62ed09ec1984
Glitch (fluffy-cliff-stitch) <none>
2024-06-25 14:28:09 +0000

    ⛈🐸 Checkpoint ./package.json:68569246/1273 ./server.js:68569246/254

--- server.js
+++ server.js
@@ -3,11 +3,19 @@
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const cors        = require('cors');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const apiRoutes         = require('./routes/api.js');
 const fccTestingRoutes  = require('./routes/fcctesting.js');
 const runner            = require('./test-runner');
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Error connecting to MongoDB:', err));
 
 const app = express();
 
