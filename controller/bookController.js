d9ca789719026ea84c180d388c7b5cd1abdb6c7e
Glitch (fluffy-cliff-stitch) <none>
2024-06-25 11:22:43 +0000

    ❣️🍀 Checkpoint ./controller/bookController.js:68569246/654

---
+++ controller/bookController.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+  title: { type: String, required: true },
+  comments: [{ type: String }],
+  commentCount: { type: Number, default: 0 },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Book', bookSchema);
