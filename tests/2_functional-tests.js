e01f120c2bf068cc8669151da73569d54dd0ab6e
Glitch (fluffy-cliff-stitch) <none>
2024-06-25 18:01:56 +0000

    🚶🦁 Checkpoint ./tests/2_functional-tests.js:68569246/1907

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -65,7 +65,19 @@ suite('Functional Tests', function() {
 
     suite('GET /api/books/[id] => book object with [id]', function(){
      test('should fail if id is not in db', (done) => {
-    chai.request(server)
+  
+        const bookId = 123;
+        chai.request(server)
+         .get(`/api/books/${bookId}`)
+         .end((err, res) => {
+            assert.equal(res.text,'')
+            done();
+          });
+    
+  });
+      
+      test('Test GET /api/books/[id] with valid id in db',  function(done){
+         chai.request(server)
      .post('/api/books')
      .send({ title: 'Test Book' })
      .end((err, res) => {
@@ -73,22 +85,17 @@ suite('Functional Tests', function() {
         chai.request(server)
          .get(`/api/books/${bookId}`)
          .end((err, res) => {
-            expect(res.body).to.be.an('object');
-            expect(res.body.commentcount).to.exist;
-            expect(res.body.title).to.exist;
-            expect(res.body._id).to.exist;
+            assert.isObject(res.body, 'response should be an object');
+        assert.property(res.body, 'commentcount', 'Books in array should contain commentcount');
+        assert.property(res.body, 'title', 'Books in array should contain title');
+        assert.property(res.body, '_id', 'Books in array should contain _id');
             done();
           });
-      });
-  });
-      
-      test('Test GET /api/books/[id] with valid id in db',  function(done){
-        //done();
       });
       
     });
 
-
+    })
     suite('POST /api/books/[id] => add comment/expect book object with id', function(){
       
       test('Test POST /api/books/[id] with comment', function(done){
